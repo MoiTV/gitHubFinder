1240a23d0bd114e269651291fd446b578ca629ef
Moises Valdez <moisesvaldez075@gmail.com>
2019-03-13 16:05:17 -0700

    updated changes

--- gitHub.js
+++ gitHub.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = 'e2d205325afb995c2a3b';
         this.client_secret = 'cb2a9d6705e2277e87473a55491eec7cc5f48d13';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
-    async getUser(user, repos) {
+    async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
